7e1e17d80e181ac462508fd170b2fd8b3a52faf7
HA KHANH LONG <HA KHANH LONG@DESKTOP-K102PK6>
2019-03-04 15:12:21 +0700

    edit source commit

--- config.py
+++ config.py
@@ -14,15 +14,15 @@ class Config:
 
 class Development(Config):
     DEBUG = True
-    #MONGO_DATABASE_SERVER = os.environ.get('DEV_DATABASE_SERVER') or 'localhost'
-    MONGO_DATABASE_SERVER = os.environ.get('DEV_DATABASE_SERVER') or '118.107.88.35'
+    MONGO_DATABASE_SERVER = os.environ.get('DEV_DATABASE_SERVER') or 'localhost'
+    #MONGO_DATABASE_SERVER = os.environ.get('DEV_DATABASE_SERVER') or '118.107.88.35'
     MONGO_DATABASE_PORT = os.environ.get('DEV_DATABASE_PORT') or 27017
     MONGO_DATABASE_NAME = os.environ.get('DEV_DATABASE_NAME') or 'DEV_BLANKSPIDER'
 
 class Production(Config):
     DEBUG = False
-    #MONGO_DATABASE_SERVER = os.environ.get('PROD_DATABASE_SERVER') or 'localhost'
-    MONGO_DATABASE_SERVER = os.environ.get('PROD_DATABASE_SERVER') or '118.107.88.35'
+    MONGO_DATABASE_SERVER = os.environ.get('PROD_DATABASE_SERVER') or 'localhost'
+    #MONGO_DATABASE_SERVER = os.environ.get('PROD_DATABASE_SERVER') or '118.107.88.35'
     MONGO_DATABASE_PORT = os.environ.get('PROD_DATABASE_PORT') or 27017
     MONGO_DATABASE_NAME = os.environ.get('PROD_DATABASE_NAME') or 'BLANKSPIDER'
 
